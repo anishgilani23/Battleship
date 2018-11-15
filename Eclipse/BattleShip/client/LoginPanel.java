45edf0fc51a5578f1fd1b0e0054fed1725df3a5d
Alex Waitkus <43049327+ANWaitkus@users.noreply.github.com>
2018-11-15 16:54:09 -0600

    Commit 1
    Commit 1

--- Eclipse/BattleShip/client/LoginPanel.java
+++ Eclipse/BattleShip/client/LoginPanel.java
@@ -1,5 +1,23 @@
 package client;
 
-public class LoginPanel {
+import javax.swing.*;
 
+public class LoginPanel extends JFrame 
+{
+	private JButton login;
+	private JTextField password;
+	private JTextField username;
+	private JLabel passLabel;
+	private JLabel userLabel;
+	
+	public LoginPanel()
+	{
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		
+		this.setSize(200,200);
+		this.setVisible(true);
+	}
 }
--- Eclipse/BattleShip/server/GameServerPanel.java
+++ Eclipse/BattleShip/server/GameServerPanel.java
@@ -1,5 +1,64 @@
 package server;
 
-public class GameServerPanel {
 
+
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.*;
+import java.io.IOException;
+import java.awt.*;
+
+
+public class GameServerPanel extends JFrame
+{
+
+	private JButton listen;
+	private JButton close;
+	private JButton stop;
+	private JLabel logLabel;
+	private JTextArea logArea;
+	
+	//private GameServerController controller;
+	
+	public GameServerPanel()
+	{
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Battleship - Server");
+		
+		//Create panels for the info log and the buttons
+		JPanel logPanel = new JPanel();
+		JPanel buttonPanel = new JPanel();
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		//Create and add the buttons to the bottom of the panel
+		listen = new JButton("Listen");
+		close = new JButton("Close");
+		stop = new JButton("Stop");
+		
+		buttonPanel.add(listen, BorderLayout.SOUTH);
+	    buttonPanel.add(close, BorderLayout.SOUTH);
+	    buttonPanel.add(stop, BorderLayout.SOUTH);
+		
+	    //Create log and add it to the panel
+		logArea = new JTextArea(20,29);
+		JScrollPane pane = new JScrollPane(logArea);
+		logPanel.add(pane);
+	    add(logPanel, BorderLayout.CENTER);
+	    
+	    //Add logLabel to frame
+	    logLabel = new JLabel("Activity Log:");
+		JPanel labelPanel = new JPanel();
+		labelPanel.add(logLabel);
+		add(labelPanel, BorderLayout.NORTH);
+	    
+	    //Sets size and visibility
+	    this.setSize(500,500);
+		this.setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		new GameServerPanel();
+	}
+	
 }
